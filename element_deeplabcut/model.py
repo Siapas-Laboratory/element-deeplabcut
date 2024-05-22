4800307b1657d568e5adbf1e434f145a4c40011d
Nathaniel <nnyema@Nathaniels-MacBook-Pro-2.siapaslab>
2024-05-22 14:45:44 -0700

    fix bug in get_trajectories where it doesn't use the session key to get body part positions

--- element_deeplabcut/model.py
+++ element_deeplabcut/model.py
@@ -804,7 +804,7 @@ class PoseEstimation(dj.Computed):
         df = None
         for body_part in body_parts:
             x_pos, y_pos, z_pos, likelihood = (
-                cls.BodyPartPosition & {"body_part": body_part}
+                cls.BodyPartPosition & {**key, "body_part": body_part}
             ).fetch1("x_pos", "y_pos", "z_pos", "likelihood")
             if not z_pos:
                 z_pos = np.zeros_like(x_pos)
