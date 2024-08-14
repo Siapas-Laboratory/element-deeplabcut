7621b2fb5c05fcaaf7d0a265d6518166624c3bb5
Nathaniel Nyema <nnyema@caltech.edu>
2024-08-13 20:31:01 -0700

    revert

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     scripts=[],
     install_requires=[
-        "datajoint @ git+https://github.com/Siapas-Laboratory/datajoint-python.git",
+        "datajoint>=0.13.0",
         "graphviz",
         "pydot",
         "ipykernel",
